adbbb99fbe64525e64ed55e491e1bdf170f34da1
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2015-12-08 16:02:40 -0300

    Troca de própiedade total para metodo GetTotal

--- Source/CaixaBanco/ATM/Cofre.cs
+++ Source/CaixaBanco/ATM/Cofre.cs
@@ -9,21 +9,21 @@ namespace Banco
 {
     class Cofre
     {
-        public int total { get; internal set; }
-        public Cedulas cedulas { get; internal set; }
+        public Cedulas cedulas { get; private set; }
 
         public Cofre()
         {
         }
         public Cofre(int notasDe100, int notasDe50, int notasDe20, int notasDe10, int notasDe5, int notasDe2)
         {
-            Adicionar(notasDe100, notasDe50, notasDe20, notasDe10, notasDe5, notasDe2);
-            total = Utilitarios.ValorDasNotas(cedulas);
+            Adicionar(notasDe100, notasDe50, notasDe20, notasDe10, notasDe5, notasDe2);            
+        }
+        public int GetTotal() {
+            return Utilitarios.ValorDasNotas(cedulas);
         }
         public void Adicionar(int valor)
         {
             cedulas = Utilitarios.ValorDoDinheiro(valor); 
-            total = Utilitarios.ValorDasNotas(cedulas);
         }
         public void Adicionar(int notasDe100, int notasDe50, int notasDe20, int notasDe10, int notasDe5, int notasDe2)
         {
@@ -33,13 +33,11 @@ namespace Banco
             cedulas.Add(Notas.Dez, notasDe10);
             cedulas.Add(Notas.Cinco, notasDe5);
             cedulas.Add(Notas.Dois, notasDe2);
-            total = Utilitarios.ValorDasNotas(cedulas);
         }
 
         public void Adicionar(Notas nota, int valor)
         {
             cedulas.Add(nota, valor);
-            total = Utilitarios.ValorDasNotas(cedulas);
         }
 
 
